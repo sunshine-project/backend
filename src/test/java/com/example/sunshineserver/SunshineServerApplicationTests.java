d4e925118268c844f6e93489e211ed7a8e67d244
Geonwoo Pack <packdev937@gmail.com>
2024-02-01 16:53:15 +0900

    initial commit

---
+++ src/test/java/com/example/sunshineserver/SunshineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sunshineserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunshineServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
