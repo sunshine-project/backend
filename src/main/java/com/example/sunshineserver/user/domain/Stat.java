205655cd44f90f2f4d1d731872fdf25c8bbd234f
Geonwoo Pack <packdev937@gmail.com>
2024-02-05 14:47:07 +0900

    refactor : Stat의 필드를 소문자로 리팩토링 한다

--- src/main/java/com/example/sunshineserver/user/domain/Stat.java
+++ src/main/java/com/example/sunshineserver/user/domain/Stat.java
@@ -2,24 +2,30 @@ package com.example.sunshineserver.user.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.AccessType;
 
-@Embeddable
 @Getter
+@Setter
+@Embeddable
+@EqualsAndHashCode
+@AccessType(AccessType.Type.FIELD)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Stat {
 
-    private int STR;
-    private int SPI;
-    private int PEA;
-    private int SKL;
+    private int str;
+    private int spi;
+    private int pea;
+    private int skl;
 
-    public Stat(int STR, int SPI, int PEA, int SKL) {
-        this.STR = STR;
-        this.SPI = SPI;
-        this.PEA = PEA;
-        this.SKL = SKL;
+    public Stat(int str, int spi, int pea, int skl) {
+        this.str = str;
+        this.spi = spi;
+        this.pea = pea;
+        this.skl = skl;
     }
 
     public static Stat of(final int STR, final int SPI, final int PEA, final int SKL) {
