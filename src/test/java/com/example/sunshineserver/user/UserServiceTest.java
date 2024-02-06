3cf8783488c9844db16986a9a74569cc4235e023
Geonwoo Pack <packdev937@gmail.com>
2024-02-06 19:22:36 +0900

    test : 홈 화면이 조회 되는지 테스트 한다

--- src/test/java/com/example/sunshineserver/user/UserServiceTest.java
+++ src/test/java/com/example/sunshineserver/user/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.example.sunshineserver.user;
 import com.example.sunshineserver.user.domain.repository.UserPort;
 import com.example.sunshineserver.user.presentation.dto.UserCreateRequest;
 import com.example.sunshineserver.user.application.UserService;
+import com.example.sunshineserver.user.presentation.dto.UserHomeResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,5 +34,27 @@ public class UserServiceTest {
 
         // then
         Assertions.assertThat(userPort.findAll()).hasSize(1);
+    }
+
+    @Test
+    void 홈_화면을_조회한다() {
+        // given
+        UserCreateRequest request = UserSteps.유저_생성_요청();
+        Long userId = userService.create(request);
+
+        // when
+        UserHomeResponse response = userService.findHome(userId);
+
+        // then
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.name()).isEqualTo(request.name());
+        Assertions.assertThat(response.characterType()).isEqualTo(request.characterType().name());
+        Assertions.assertThat(response.stat()).isEqualTo(request.stat());
+        Assertions.assertThat(response.experiencePoint()).isEqualTo(0);
+        Assertions.assertThat(response.level()).isEqualTo(1);
+        Assertions.assertThat(response.exclusiveRange()).isEqualTo(1000);
+        Assertions.assertThat(response.leftDay()).isEqualTo(70);
+        Assertions.assertThat(response.isQuestExisted()).isFalse();
+        Assertions.assertThat(response.isAbleToEndGame()).isFalse();
     }
 }
