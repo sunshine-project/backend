15e014400f1698fd3326e240a6ffffcf5d87ce75
Geonwoo Pack <packdev937@gmail.com>
2024-02-07 03:07:33 +0900

    feat : UserController에 API를 추가한다

--- src/main/java/com/example/sunshineserver/user/presentation/UserController.java
+++ src/main/java/com/example/sunshineserver/user/presentation/UserController.java
@@ -3,11 +3,13 @@ package com.example.sunshineserver.user.presentation;
 import com.example.sunshineserver.global.domain.RequestURI;
 import com.example.sunshineserver.user.presentation.dto.UserCreateRequest;
 import com.example.sunshineserver.user.application.UserService;
+import com.example.sunshineserver.user.presentation.dto.UserHomeResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,15 @@ public class UserController {
     })
     public void create(@RequestBody UserCreateRequest request) {
         userService.create(request);
+    }
+
+    @GetMapping
+    @Operation(summary = "홈 화면 조회", description = "홈 화면을 조회합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "404", description = "유저를 찾을 수 없습니다."),
+    })
+    public UserHomeResponse findHome(Long userId) {
+        return userService.findHome(userId);
     }
 }
