a8b6834615d6a8037099ab85d5898e8c19f01a72
Geonwoo Pack <packdev937@gmail.com>
2024-02-16 02:41:59 +0900

    feat : id_token에서 이메일 정보를 추출한다

--- src/main/java/com/example/sunshineserver/auth/domain/GoogleAuthenticator.java
+++ src/main/java/com/example/sunshineserver/auth/domain/GoogleAuthenticator.java
@@ -4,32 +4,49 @@ import com.example.sunshineserver.auth.presentation.dto.AccessTokenResponse;
 import com.example.sunshineserver.auth.presentation.dto.UserResourceResponse;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RequiredArgsConstructor
+@Component
 public class GoogleAuthenticator {
 
-    private final GoogleProperties googleProperties;
+    @Value("${oauth.google.client-id}")
+    private String clientId;
+    @Value("${oauth.google.client-secret}")
+    private String clientSecret;
+    @Value("${oauth.google.redirect-uri}")
+    private String redirectUri;
+    @Value("${oauth.google.token-uri}")
+    private String tokenUri;
+    @Value("${oauth.google.token-info-uri}")
+    private String tokenInfoUri;
+    @Value("${oauth.google.resource-uri}")
+    private String resourceUri;
 
     public AccessTokenResponse getAccessToken(String authCode) {
         WebClient webClient = WebClient.create();
 
         Mono<AccessTokenResponse> mono = webClient.post()
-            .uri(googleProperties.getTokenUri())
+            .uri(tokenUri)
             .contentType(MediaType.APPLICATION_FORM_URLENCODED)
             .body(BodyInserters.fromFormData("code", authCode)
-	.with("client_id", googleProperties.getClientId())
-	.with("client_secret", googleProperties.getClientSecret())
-	.with("redirect_uri", googleProperties.getRedirectUri())
+	.with("client_id", clientId)
+	.with("client_secret", clientSecret)
+	.with("redirect_uri", redirectUri)
 	.with("grant_type", "authorization_code"))
             .retrieve()
             .bodyToMono(JsonNode.class)
+//            .doOnNext(jsonNode -> log.info("Received JsonNode: " + jsonNode)) // JsonNode 확인
             .map(jsonNode -> new AccessTokenResponse(jsonNode.get("access_token").asText(),
-	jsonNode.get("refresh_token").asText()));
+	jsonNode.get("id_token").asText()));
 
         return mono.block();
     }
@@ -38,11 +55,25 @@ public class GoogleAuthenticator {
         WebClient webClient = WebClient.create();
 
         Mono<UserResourceResponse> mono = webClient.get()
-            .uri("oauth2." + googleProperties.getResourceUri() + ".resource-uri")
+            .uri(resourceUri)
             .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
             .retrieve()
             .bodyToMono(JsonNode.class)
             .map(jsonNode -> new UserResourceResponse(jsonNode.get("email").asText()));
+
+        return mono.block();
+    }
+
+    public String parseIdToken2Email(String idToken) {
+        WebClient webClient = WebClient.create();
+
+        Mono<String> mono = webClient.post()
+            .uri(tokenInfoUri)
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(BodyInserters.fromFormData("id_token", idToken))
+            .retrieve()
+            .bodyToMono(JsonNode.class)
+            .map(jsonNode -> jsonNode.get("email").asText());
 
         return mono.block();
     }
