b61634caf6cbe640da837a7aa2d2c12d5e195981
Geonwoo Pack <packdev937@gmail.com>
2024-02-16 12:08:20 +0900

    refactor : Swagger 주소를 변경한다

--- src/main/java/com/example/sunshineserver/global/config/SwaggerConfig.java
+++ src/main/java/com/example/sunshineserver/global/config/SwaggerConfig.java
@@ -2,7 +2,10 @@ package com.example.sunshineserver.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
@@ -18,9 +21,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "Authorization";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-            .servers(List.of(new Server().url("http://localhost:8080/").description("Sunshine API Server")));
+            .components(new Components()
+	.addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+	    .type(SecurityScheme.Type.HTTP)
+	    .name(SECURITY_SCHEME_NAME)
+	    .scheme("Bearer")
+	    .bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+
+            .servers(List.of(
+	new Server().url("http://35.216.8.32:8080/").description("Sunshine API Server")));
     }
 }
